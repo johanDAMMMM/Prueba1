d2d0806911ca0941661f1072bc063f9d75a2f1cf
johanDAMMMM <johandavidarguellomedellin@gmail.com>
2024-02-08 17:33:43 -0500

    Proyecto

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function Tv(){
+    window.location.href="TV.html"
+}
+
+function Pelicula(){
+    window.location.href="Pelicula.html"
+}
+
+function Musica(){
+    window.location.href="Musica.html"
+}
+
+function VideoMusica(){
+    window.location.href="VideoMusica.html"
+}
+
+function Juego(){
+    window.location.href="Juego.html"
+}
+
+function Libro(){
+    window.location.href="Libro.html"
+}
