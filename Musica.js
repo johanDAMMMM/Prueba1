a594cbecedf2fe3a5e6ab5e05e46b870b6fd5c4a
johanDAMMMM <johandavidarguellomedellin@gmail.com>
2024-02-20 17:35:01 -0500

    DAM

--- Musica.js
+++ Musica.js
@@ -3,6 +3,9 @@ function mostrarAudio(){
     var output = document.getElementById("nombreArchivo");
     var audio = document.getElementById("reproductorAudio");
     var contenedorAudio = document.getElementById("contenedorAudio");
+    var generoInput = document.getElementById("generoInput").value;
+    var nombreCancionInput = document.getElementById("nombreCancionInput").value;
+    var descripcionInput = document.getElementById("descripcionInput").value;
 
     output.textContent = "Archivo seleccionado: " +input.files[0].name;
 
@@ -10,5 +13,9 @@ function mostrarAudio(){
     audio.src = fileURL;
 
     contenedorAudio.style.display = "block";
+
+    console.log("Genero:",generoInput);
+    console.log("Nombre de la canción:",nombreCancionInput);
+    console.log("Descripción:",descripcionInput);
 
 }
